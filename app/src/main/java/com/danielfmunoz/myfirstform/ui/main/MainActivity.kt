9f6d61b43e06e6c9043bdfb23b276f2e04286af3
DANIEL MUNOZ <dafemure.180@gmail.com>
2023-03-11 10:33:55 -0500

    Carga del ejercicio 2 del laboratorio 1 version1, este se enfoca en el diseno de interfaz

---
+++ app/src/main/java/com/danielfmunoz/myfirstform/ui/main/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.danielfmunoz.myfirstform.ui.main
+
+
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.danielfmunoz.myfirstform.R
+import com.danielfmunoz.myfirstform.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
+
+        mainBinding.btnGuardar.setOnClickListener {
+            validarCampos()
+        }
+    }
+
+    private fun validarCampos() {
+        val nombre = mainBinding.etNombre.text.toString()
+        val correo = mainBinding.etCorreo.text.toString()
+        val password = mainBinding.etPassword.text.toString()
+        val repetirPassword = mainBinding.etRepetirPassword.text.toString()
+
+        if (nombre.isEmpty() || correo.isEmpty() || password.isEmpty() || repetirPassword.isEmpty()) {
+            Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+            mainBinding.tvResultado.text = ""
+        } else if (password != repetirPassword) {
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            mainBinding.tvResultado.text = ""
+        } else {
+            val sexo = if (mainBinding.radioSexo.checkedRadioButtonId == R.id.rb_femenino) "Femenino" else "Masculino"
+            val hobbies = mutableListOf<String>()
+            val lugarNacimiento = mainBinding.spinnerLugarNacimiento.selectedItem.toString()
+            if (mainBinding.cbHobby1.isChecked) hobbies.add(mainBinding.cbHobby1.text.toString())
+            if (mainBinding.cbHobby2.isChecked) hobbies.add(mainBinding.cbHobby2.text.toString())
+            if (mainBinding.cbHobby3.isChecked) hobbies.add(mainBinding.cbHobby3.text.toString())
+            if (mainBinding.cbHobby4.isChecked) hobbies.add(mainBinding.cbHobby4.text.toString())
+
+            var datos = "Nombre: $nombre\n"
+            datos += "Correo electrónico: $correo\n"
+            datos += "Sexo: $sexo\n"
+            if (hobbies.isNotEmpty()) {
+                datos += "Hobbies: ${hobbies.joinToString(", ")}\n"
+            }
+            datos += "Ciudad: $lugarNacimiento\n"
+            mainBinding.tvResultado.text = datos
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/danielfmunoz/myfirstform/ui/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.danielfmunoz.myfirstform.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.danielfmunoz.myfirstform.R
+import com.danielfmunoz.myfirstform.ui.main.MainActivity
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val timer = Timer()
+        timer.schedule(
+            timerTask {
+            var intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },
+            2000
+        )
+    }
+}
